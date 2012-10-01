f2dacd7e0e89b61308b6afe5ff378bea8da084b7
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-01 13:27:02 -0600

    Updated

---
+++ source/prep/collections/ReadOnlySet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace prep.collections
+{
+  public class ReadOnlySet<T> : IEnumerable<T>
+  {
+    IList<T> items;
+
+    public ReadOnlySet(IList<T> items)
+    {
+      this.items = items;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      return items.GetEnumerator();
+    }
+  }
+}
