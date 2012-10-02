5576042dc04105cae0fb2ecc1c959b05f18c4a76
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-01 18:33:09 -0600

    Updated

--- source/prep/collections/Movie.cs
+++ source/prep/collections/Movie.cs
@@ -10,6 +10,7 @@ namespace prep.collections
     public ProductionStudio production_studio { get; set; }
     public Genre genre { get; set; }
     public int rating { get; set; }
+
     public DateTime date_published { get; set; }
 
     public override bool Equals(object obj)
--- source/prep/utility/filtering/Where.cs
+++ source/prep/utility/filtering/Where.cs
@@ -1,10 +1,33 @@
+using prep.collections;
+
 namespace prep.utility.filtering
 {
   public class Where<ItemToFilter>
   {
-    public static PropertyAccessor<ItemToFilter,PropertyType> has_a<PropertyType>(PropertyAccessor<ItemToFilter,PropertyType> accessor)
+    public static MatchFactory<ItemToFilter, PropertyType> has_a<PropertyType>(
+      PropertyAccessor<ItemToFilter, PropertyType> accessor)
     {
-      return accessor;
+      return new MatchFactory<ItemToFilter, PropertyType>(accessor);
+    }
+  }
+
+  public class MatchFactory<Item, PropertyType>
+  {
+    PropertyAccessor<Item, PropertyType> accessor;
+
+    public MatchFactory(PropertyAccessor<Item, PropertyType> accessor)
+    {
+      this.accessor = accessor;
+    }
+
+    public IMatchAn<Item> equal_to(PropertyType value)
+    {
+      return new DynamicCondition<Item>(x => accessor(x).Equals(value));
+    }
+
+    public IMatchAn<Movie> equal_to_any(params PropertyType[] values)
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
